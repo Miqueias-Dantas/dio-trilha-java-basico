7541b7b699b3fa6fff1b859d1fce05984f5190f3
Miqueias-Dantas <miqueias3x@gmail.com>
2024-10-06 17:55:18 -0300

    Conta Terminal banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o sua agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Digite o número da sua conta:");
+        int conta = scanner.nextInt();
+        
+        
+        System.out.println("Por favor, digite o seu saldo:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e o seu saldo "+saldo+" já está a disponivel para saque");
+        
+        scanner.close();
     }
 }
